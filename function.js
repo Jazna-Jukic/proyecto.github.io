993b1959a6b0328e7596f10acc60f64ce5ff30fe
Jazna-Jukic <124108481+Jazna-Jukic@users.noreply.github.com>
2023-01-31 22:22:20 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById('boton_color').addEventListener('click', function() {
+ document.body.style.backgroundColor = '#CCFFCC';
+});
+
+document.getElementById('boton_default').addEventListener('click', function() {
+    document.body.style.backgroundColor ='#e7fda8';
+});
